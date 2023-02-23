2b4c97897c76b72b0c40d9d6a509ea49a2f9b0f6
sharankan12 <126036345+sharankan12@users.noreply.github.com>
2023-02-23 07:06:51 +0530

    Add files via upload

---
+++ projet.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class projet{
+     public static void main(String[] args) {
+            Scanner scanner =new Scanner(System.in);
+            int input =scanner.nextInt();
+            if(input<0){
+                System.out.print("negative");
+
+            }
+            else
+            {
+                System.out.print("positive");
+            }
+        
+    }
+
+
+ }
